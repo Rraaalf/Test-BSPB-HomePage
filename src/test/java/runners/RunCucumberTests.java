7dcb449f6ac8b6da01f89508747f282d41f364d3
I <I@mac.local>
2023-11-15 09:05:43 +0300

    реализован запуск тестов Cucumber через RunCucumberTests.java

--- src/test/java/runners/RunCucumberTests.java
+++ src/test/java/runners/RunCucumberTests.java
@@ -1,7 +1,11 @@
 package runners;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@Cucumber
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
 public class RunCucumberTests {
 }
