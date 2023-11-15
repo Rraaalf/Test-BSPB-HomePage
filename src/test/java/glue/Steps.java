286060c5c445591d0d93c98f93ef5da6592a8697
I <I@mac.local>
2023-11-15 17:22:15 +0300

    if заменены на map; добавлены методы для дополнения отчетов скриншотами; добавлен проект для SoftAssertions

--- src/test/java/glue/Steps.java
+++ src/test/java/glue/Steps.java
@@ -9,33 +9,62 @@ import io.cucumber.java.ru.Тогда;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Attachment;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.assertj.core.api.SoftAssertions;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import pages.HomePage;
-import pages.LoginPage;
-import pages.Manager;
-import pages.RetailPage;
+import pages.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Steps {
 
     protected static WebDriver driver;
+    protected BusinessPage businessPage = new BusinessPage(driver);
+    protected DebitPage debitPage = new DebitPage(driver);
+    protected FinancePage financePage = new FinancePage(driver);
+    protected ForeignTradePage foreignTradePage = new ForeignTradePage(driver);
     protected HomePage homePage = new HomePage(driver);
+    protected InvestorsPage investorsPage = new InvestorsPage(driver);
     protected LoginPage loginPage = new LoginPage(driver);
+    protected PrivateBankingPage privateBankingPage = new PrivateBankingPage(driver);
     protected RetailPage retailPage = new RetailPage(driver);
+    protected SoftAssertions softly = new SoftAssertions();
+
+    protected Map<String, By> buttons = new HashMap<>();
+
+    {
+        buttons.put("Частным клиентам", homePage.RETAIL_BUTTON);
+        buttons.put("Бизнесу", homePage.BUSINESS_BUTTON);
+        buttons.put("ВЭД", homePage.FOREIGN_TRADE_BUTTON);
+        buttons.put("Финансовые рынки", homePage.FINANCE_BUTTON);
+        buttons.put("Private Banking", homePage.PRIVATE_BANKING_BUTTON);
+        buttons.put("Инвесторам", homePage.INVESTORS_BUTTON);
+        buttons.put("Войти", homePage.LOGIN_BUTTON);
+    }
+
+    protected Map<String, String> urls = new HashMap<>();
+
+    {
+        urls.put("Частным клиентам", retailPage.RETAIL_URL);
+        urls.put("Бизнесу", businessPage.BUSINESS_URL);
+        urls.put("ВЭД", foreignTradePage.FOREIGN_TRADE_URL);
+        urls.put("Финансовые рынки", financePage.FINANCE_URL);
+        urls.put("Private Banking", privateBankingPage.PRIVATE_BANKING_URL);
+        urls.put("Инвесторам", investorsPage.INVESTORS_URL);
+        urls.put("Авторизация", loginPage.LOGIN_URL);
+    }
 
     protected static void fluentWait(WebDriver driver, String url) {
         FluentWait<WebDriver> wait = new FluentWait<>(driver)
@@ -50,11 +79,10 @@ public class Steps {
         String path;
         try {
             WebDriver webDriver = new Augmenter().augment(driver);
-            File source = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.FILE);
+            File source = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
             path = "src/test/resources/attachments/screenshots/" + source.getName();
             FileUtils.copyFile(source, new File(path));
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             path = "Failed to capture screenshot: " + e.getMessage();
         }
         return path;
@@ -83,46 +111,44 @@ public class Steps {
         driver.get("https://www.bspb.ru/");
     }
 
-    @И("отображается кнопка {string}")
-    public void отображаетсяКнопка(String arg0) throws IOException {
-        attachScreenshotAllure(captureScreen());
-        if (arg0.contains("Частным клиентам")) {
-            assertThat(!driver.findElement(homePage.RETAIL_BUTTON).isDisplayed())
-                    .as("Проверка отображения кнопки {string}")
-                    .isTrue();
-        }
-        if (arg0.contains("Войти")) {
-            assertThat(driver.findElement(homePage.LOGIN_BUTTON).isDisplayed())
-                    .as("Проверка отображения кнопки {string}")
-                    .isTrue();
-        }
-    }
+//    @И("отображаются кнопки")
+//    public void отображаютсяКнопки(List<String> button) {
+//        for (String b : button) {
+//            softly.assertThat(driver.findElement(buttons.get(b)).isDisplayed())
+//                    .as("Проверка отображения кнопки \"%s\"", b)
+//                    .isFalse();
+////            System.out.println(b);
+////            softly.assertThat(driver.findElement(By.partialLinkText(b)))
+////                    .as("Проверка отображения кнопки \"%s\"", b)
+////                    .isEqualTo(driver.findElement(buttons.get(b)).getText());
+//        }
+//    }
+
+//    @И("отображается кнопка {string}")
+//    public void отображается(String button) {
+//        assertThat(driver.findElement(buttons.get(button)).isDisplayed())
+//                .as("Проверка отображения кнопки \"%s\"", button)
+//                .isTrue();
+//    }
 
     @Когда("пользователь нажимает на кнопку {string}")
-    public void пользовательНажимаетНаКнопку(String arg0) {
-        if (arg0.contains("Частным клиентам")) {
-            driver.findElement(homePage.RETAIL_BUTTON).click();
-        }
-        if (arg0.contains("Войти")) {
-            driver.findElement(homePage.LOGIN_BUTTON).click();
-        }
+    public void пользовательНажимаетНаКнопку(String button) {
+        driver.findElement(buttons.get(button)).click();
     }
 
     @Тогда("открывается страница {string}")
-    public void открываетсяСтраница(String arg0) {
-        if (arg0.contains("Частным клиентам")) {
-            driver.findElement(homePage.RETAIL_BUTTON).click();
-            fluentWait(driver, retailPage.RETAIL_URL);
-            assertThat(driver.getCurrentUrl())
-                    .as("Проверка URL страницы \"Частным клиентам\"")
-                    .isEqualTo(retailPage.RETAIL_URL);
-        }
-        if (arg0.contains("Авторизация")) {
+    public void открываетсяСтраница(String url) {
+        try {
             Manager manager = new Manager(driver);
             manager.switchToSecondTab();
             assertThat(driver.getCurrentUrl())
                     .as("Проверка, что URL страницы авторизации начинается с \"%s\"", loginPage.LOGIN_URL)
                     .startsWith(loginPage.LOGIN_URL);
+        } catch (IndexOutOfBoundsException e) {
+            fluentWait(driver, urls.get(url));
+            assertThat(driver.getCurrentUrl())
+                    .as("Проверка URL страницы \"%s\"", url)
+                    .isEqualTo(urls.get(url));
         }
     }
 
